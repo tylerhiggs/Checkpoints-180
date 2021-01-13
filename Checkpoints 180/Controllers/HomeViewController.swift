0308c98151689457a76769535d2c57dc2cada1a6
Tyler Higgs <thiggs@mit.edu>
2021-01-13 17:33:09 -0600

    gives ability ot find current longitude and latitude

--- Checkpoints 180/Controllers/HomeViewController.swift
+++ Checkpoints 180/Controllers/HomeViewController.swift
@@ -7,10 +7,13 @@
 
 import UIKit
 import GoogleSignIn
+import CoreLocation
 
 class HomeViewController: UIViewController {
     
     private var user: GIDGoogleUser?
+    private let locationManager = CLLocationManager()
+
     
     override func viewDidLoad() {
         view.backgroundColor = K.Color.light
@@ -20,17 +23,50 @@ class HomeViewController: UIViewController {
         
         self.navigationItem.setHidesBackButton(true, animated: false)
         self.navigationItem.title = "Checkpoints 180"
-        //self.navigationController?.navigationBar.frame = CGRect(x: 0, y: 0, width: self.view.bounds.width, height: 500)
-//        self.navigationController?.navigationBar.backgroundColor = K.Color.pink
+
         let font = UIFont(name: K.fontName, size: 30)!
         self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.font: font,
                                                                         NSAttributedString.Key.foregroundColor: K.Color.light]
+        
+        
+        // Location
+        
+        locationManager.delegate = self
+        
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation()
 
     }
     
     
     func setUser(_ u: GIDGoogleUser!) {
         self.user = u
+    }
+    
+    
+}
+
+
+//MARK: - Location Delagate
+
+extension HomeViewController: CLLocationManagerDelegate {
+    
+    
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            locationManager.stopUpdatingLocation()
+            let lat = location.coordinate.latitude
+            let lon = location.coordinate.longitude
+            
+            // do stuff with lat and lon
+            print(lat,lon)
+            
+        }
+    }
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+        print("ERRRRRRRRRROOORRRRRRR")
     }
     
 }
